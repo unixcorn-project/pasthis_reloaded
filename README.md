fb43b213c1e3de9438d52af1b4e0747028fcb056
Mathias B <mathias@member.fsf.org>
2016-03-19 13:45:35 +0100

    populate

---
+++ README.md
@@ -0,0 +1,105 @@
+# *Pasthis*
+
+![screenshot](http://pix.unixcorn.org/image/1455227933/original.jpg)
+
+## Stupid Simple Pastebin
+French translated and slighty improved version of [Pasthis](https://github.com/moulecorp/pasthis), it is a simple pastebin written in [php](https://www.php.net/)
+using [sqlite](https://sqlite.org/) as database backend and
+[prettify](https://code.google.com/p/google-code-prettify/) to handle the syntax
+highlighting.
+
+## Deployment
+1. Download [Pasthis](https://github.com/moulecorp/pasthis).
+2. Put it in a directory on your web server.
+3. Configure the web server:
+  - Apache: edit the RewriteBase directive in the
+    [.htaccess](https://github.com/moulecorp/pasthis/blob/master/.htaccess) if
+    needed.
+  - Nginx: see the provided
+    [nginx.conf](https://github.com/moulecorp/pasthis/blob/master/nginx.conf).
+4. Make sure that the folder is _readable_ and _writable_ by www-data, since
+this is required by php to be able to create the sqlite database.
+
+It is recommended to call the cron method on a regular basis to avoid an
+unwanted growing database and to delete expired pastes (also as a security
+concern). To do this on a GNU/Linux machine edit the /etc/crontab file and add
+the following line:
+
+		@daily www-data php /path/to/pasthis/index.php
+
+Be aware expired pastes are deleted when requested or when the cron method is
+called. Without the previous cron configuration, their deletion can't be
+ensured. They just won't be displayed.
+
+## Update
+1. Update to the latest version (keep the database!)
+2. Run update.php
+
+## Tips
+### Command line tool
+
+A [command line tool](https://github.com/moulecorp/pasthis/blob/master/pasthis.pl)
+is available allowing you to send pastes from the console standard input (STDIN)
+or from a file. In order to take advantage of this tool, download it, make it
+executable and display the help output for more information:
+
+		chmod +x ./pasthis.pl
+		./pasthis.pl --help
+		./pasthis.pl --url http://www.example.net/pasthis/ --file paste.txt
+
+You can set a default url by editing the line *my $url = undef;*
+
+### Tabulations
+
+Tabulations are handled in the textarea allowing you to write directly into
+Pasthis without changing the textarea focus.
+
+## Specifications:
+  - Pasthis MUST supports color highlighting
+  - Pasthis SHOULD be able to work without JS if necessary
+  - Pasthis MUST be lightweight
+  - Pasthis MUST NOT use clunky-javascript-powered-crypo
+  - Pasthis SHOULD NOT use predictable (by a casual attacker) pastes url
+  - Pasthis MUST deter trivial spam attacks
+  - Pasthis MUST have a way to delete outdated pastes without user intervention
+  - Pasthis MUST allow the user to see the raw content
+  - Pasthis MUST NOT store users IP in plain-text
+  - Pasthis MUST NOT depend on external services
+
+## Implementation
+### Anti-spam
+Every time a paste is sent, a value (called degree) is associated to
+the poster's ip hash. It is used in the following formula:
+
+    T = time() + intval(pow(degree, 2.5))
+
+If the user posts another paste after T, the degree is reset to zero.
+If he tries before T, the degree is incremented, and the paste is denied.
+
+There is also an hidden field, that set the degree to 512 (Which corresponds
+to ~72h) if filled.
+
+
+### Display
+The user can access the raw version of a paste by appending
+@raw to its id.
+
+## Authors and License
+
+		Copyright (C) 2014 - 2015 Julien (jvoisin) Voisin - dustri.org
+		Copyright (C) 2014 - 2015 Antoine Tenart <antoine.tenart@ack.tf>
+
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU General Public License
+		as published by the Free Software Foundation; either version 2
+		of the License, or (at your option) any later version.
+    
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+		GNU General Public License for more details.
+    
+		You should have received a copy of the GNU General Public License
+		along with this program; if not, write to the Free Software
+		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+		02110-1301, USA.
---
+++ index.php
@@ -0,0 +1,320 @@
+<?php
+/**
+ *  Pasthis - Stupid Simple Pastebin
+ *
+ * Copyright (C) 2014 - 2015 Julien (jvoisin) Voisin - dustri.org
+ * Copyright (C) 2014 - 2015 Antoine Tenart <antoine.tenart@ack.tf>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+final class Pasthis {
+    public $title;
+    private $contents = array ();
+    private $db;
+
+    function __construct ($title = 'Pasthis') {
+        $this->title = $title;
+        $dsn = 'sqlite:' . dirname(__FILE__) .'/pasthis.db';
+        try {
+            $this->db = new PDO ($dsn);
+            $this->db->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            $this->db->setAttribute (PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (PDOException $e) {
+            die ('Unable to open database: ' . $e->getMessage ());
+        }
+        $this->db->exec ('pragma auto_vacuum = 1');
+        $this->db->exec (
+            "CREATE TABLE if not exists pastes (
+                id PRIMARY KEY,
+                deletion_date TIMESTAMP,
+                highlighting INTEGER,
+                wrap INTEGER,
+                paste BLOB
+            );"
+        );
+        $this->db->exec (
+            "CREATE TABLE if not exists users (
+                hash PRIMARY KEY,
+                nopaste_period TIMESTAMP,
+                degree INTEGER
+            );"
+        );
+    }
+
+    function __destruct () {
+        $this->db = null;
+    }
+
+    private function add_content ($content, $prepend = false) {
+        if (!$prepend)
+            $this->contents[] = $content;
+        else
+            array_unshift ($this->contents, $content);
+    }
+
+    private function render () {
+        print '<!DOCTYPE html>';
+        print '<html>';
+        print '<head>';
+        print '<title>'.htmlentities ($this->title).'</title>';
+        print '<link href="./css/style.css" rel="stylesheet" type="text/css" />';
+        print '<link href="./css/prettify.css" rel="stylesheet" type="text/css" />';
+        print '<link href="./css/skeleton.css"rel="stylesheet" type="text/css" />';
+        print '<link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">';
+        print '<meta name="viewport" content="width=device-width, initial-scale=1">';
+        print '</head>';
+        print '<div class="container" style="margin-top: 3%">';
+        print '<h1>Pasthis</h1>';
+        print '<div id="common">';
+        print '<h4>Partage de presse-papier</h4>';
+        print '<body>';
+        while (list (, $ct) = each ($this->contents))
+            print $ct;
+        print '<div id="footer">';
+        print 'Propulsé par <a href="http://pasthis.moulecorp.org">pasthis</a> - ';
+        print '<a href="./pasthis.pl">outil en ligne de commande</a>';
+        print '</div>';
+        print '</div>';
+        print '</body>';
+        print '</html>';
+        exit ();
+    }
+
+    private function remaining_time ($timestamp) {
+        if ($timestamp == -1)
+            return "N'expire jamais.";
+        elseif ($timestamp == 0)
+            return 'Expirée.';
+        elseif ($timestamp == -2)
+            return 'Une lecture restante.';
+
+        $format = function ($t,$s) { return $t ? $t.' '.$s.($t>1 ? 's' : '' ).' ' : ''; };
+
+        $expiration = new DateTime ('@'.$timestamp);
+        $interval = $expiration->diff (new DateTime (), true);
+
+        $ret = 'Expire dans '.$format ($interval->days, 'jours');
+        if ($interval->days < 31) {
+            $ret .= $format ($interval->h, 'heures');
+            if ($interval->d === 0) {
+                $ret .= $format ($interval->i, 'minutes');
+                if ($interval->h === 0)
+                    $ret .= $format ($interval->s, 'secondes');
+            }
+        }
+        return rtrim ($ret).'.';
+    }
+
+    function prompt_paste () {
+        $this->add_content (
+            '<form method="post" action=".">
+                <label for="d">Expiration: </label>
+                <select name="d" id="d">
+                    <option value="-2">détruire après lecture</option>
+                    <option value="600">10 minutes</option>
+                    <option value="3600">1 heure</option>
+                    <option value="86400" selected="selected">1 jour</option>
+                    <option value="604800">1 semaine</option>
+                    <option value="-1">éternelle</option>
+                </select>
+                <input type="text" id="ricard" name="ricard"
+                        placeholder="Do not fill me!" />
+                <input type="submit" value="Envoyer">
+                <span id="left">
+                <input type="checkbox" id="wrap" name="wrap"> Casser les lignes trop longues<br />
+                <input type="checkbox" id="highlighting" name="highlighting"> Activer la coloration syntaxique<br />
+                </span>
+                <textarea autofocus required name="p"></textarea>
+            </form>'
+        );
+        $this->add_content ('<script src="./js/textarea.js"></script>');
+
+        $this->render ();
+    }
+
+    private function generate_id () {
+        $query = $this->db->prepare (
+            "SELECT id FROM pastes
+             WHERE id = :uniqid;"
+        );
+        $query->bindParam (':uniqid', $uniqid, PDO::PARAM_STR, 6);
+
+        do {
+            $uniqid = substr (uniqid (), -6);
+            $query->execute ();
+        } while ($query->fetch () != false);
+
+        return $uniqid;
+    }
+
+    private function nopaste_period ($degree) {
+        return time () + intval (pow ($degree, 2.5));
+    }
+
+    private function check_spammer () {
+        $hash = sha1 ($_SERVER['REMOTE_ADDR']);
+
+        $query = $this->db->prepare (
+            "SELECT * FROM users
+             WHERE hash = :hash"
+        );
+        $query->bindValue (':hash', $hash, PDO::PARAM_STR);
+        $query->execute ();
+        $result = $query->fetch ();
+
+        $in_period = (!empty ($result) and time () < $result['nopaste_period']);
+        $obvious_spam = (!isset ($_POST['ricard']) or !empty ($_POST['ricard']));
+
+        $degree = $in_period ? $result['degree']+1 : ($obvious_spam ? 512 : 1);
+        $nopaste_period = $this->nopaste_period ($degree);
+
+        $query = $this->db->prepare (
+            "REPLACE INTO users
+             (hash, nopaste_period, degree)
+             VALUES (:hash, :nopaste_period, :degree);"
+        );
+        $query->bindValue (':hash', $hash, PDO::PARAM_STR);
+        $query->bindValue (':nopaste_period', $nopaste_period, PDO::PARAM_INT);
+        $query->bindValue (':degree', $degree, PDO::PARAM_INT);
+        $query->execute ();
+
+        if ($in_period or $obvious_spam)
+            die ('Spam');
+    }
+
+    function add_paste ($deletion_date, $highlighting, $wrap, $paste) {
+        $this->check_spammer();
+
+        $deletion_date = intval ($deletion_date);
+
+        if ($deletion_date > 0)
+            $deletion_date += time ();
+
+        $uniqid = $this->generate_id ();
+
+        $query = $this->db->prepare (
+            "INSERT INTO pastes (id, deletion_date, highlighting, wrap, paste)
+             VALUES (:uniqid, :deletion_date, :highlighting, :wrap, :paste);"
+        );
+        $query->bindValue (':uniqid', $uniqid, PDO::PARAM_STR);
+        $query->bindValue (':deletion_date', $deletion_date, PDO::PARAM_INT);
+        $query->bindValue (':highlighting', $highlighting, PDO::PARAM_INT);
+        $query->bindValue (':wrap', $wrap, PDO::PARAM_INT);
+        $query->bindValue (':paste', $paste, PDO::PARAM_STR);
+        $query->execute ();
+
+        header ('location: ./' . $uniqid);
+    }
+
+    function show_paste ($param) {
+        $id = str_replace ("@raw", "", $param);
+        $is_raw = intval(strtolower (substr ($param, -4)) == "@raw");
+
+        $fail = false;
+        $query = $this->db->prepare(
+            "SELECT * FROM pastes
+             WHERE id = :id;"
+        );
+        $query->bindValue (':id', $id, PDO::PARAM_STR);
+        $query->execute ();
+        $result = $query->fetch ();
+
+        if ($result == null) {
+            $fail = true;
+        } elseif ($result['deletion_date'] < time ()
+                and $result['deletion_date'] >= 0) {
+            $query = $this->db->prepare (
+                "DELETE FROM pastes
+                 WHERE id = :id;"
+            );
+            $query->bindValue (':id', $id, PDO::PARAM_STR);
+            $query->execute ();
+
+            /* do not fail on "burn after reading" pastes */
+            if ($result['deletion_date'] != 0)
+                $fail = true;
+        } elseif ($result['deletion_date'] == -2) {
+            $query = $this->db->prepare (
+                "UPDATE pastes
+                 SET deletion_date=0
+                 WHERE id = :id;"
+            );
+            $query->bindValue (':id', $id, PDO::PARAM_STR);
+            $query->execute ();
+        }
+
+        if ($fail) {
+            $this->add_content ('<p>Pas de presse-papier pour cet ID :(</p>');
+            $this->prompt_paste ();
+        } else {
+            header ('X-Content-Type-Options: nosniff');
+
+            if ($is_raw) {
+                header ('Content-Type: text/plain; charset=utf-8');
+
+                print $result['paste'];
+                exit ();
+            } else {
+                header ('Content-Type: text/html; charset=utf-8');
+
+                if ($result['highlighting']) {
+                    $this->add_content ('<script>window.onload=function(){prettyPrint();}</script>');
+                    $this->add_content ('<script src="./js/prettify.js"></script>', true);
+                }
+                $this->add_content (
+                    '<div id="links">
+                         <a href="./'.$id.'@raw">Données brutes</a> - <a href="./">Nouveau</a>
+                         <span id="left">'.$this->remaining_time ($result['deletion_date']).'</span>
+                     </div>'
+                );
+                $class = 'prettyprint linenums';
+                if ($result['wrap'])
+                    $class .= ' wrap';
+
+                $this->add_content ('<pre class="'.$class.'">'.htmlentities ($result['paste']).'</pre>');
+            }
+        }
+
+        $this->render ();
+    }
+
+    function cron () {
+        $this->db->exec (
+            "DELETE FROM pastes
+             WHERE deletion_date > 0
+             AND strftime ('%s','now') > deletion_date;
+             DELETE FROM users
+             WHERE strftime ('%s','now') > nopaste_period;"
+        );
+    }
+}
+
+$pastebin = new Pasthis ();
+
+if (php_sapi_name () == 'cli') {
+    $pastebin->cron ();
+    exit ();
+}
+
+if (isset ($_GET['p']))
+    $pastebin->show_paste ($_GET['p']);
+elseif (isset ($_POST['d']) && isset ($_POST['p']))
+    $pastebin->add_paste ($_POST['d'], isset ($_POST['highlighting']),
+                          isset ($_POST['wrap']), $_POST['p']);
+else
+    $pastebin->prompt_paste ();
+?>
